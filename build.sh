096122e635b5d1dd88399a97f796cc8a2e9d9e2f
Lucka <lpdrijfhout@gmail.com>
2020-11-26 12:30:29 +0200

    UPDATE version & build trigger

--- build.sh
+++ build.sh
@@ -4,10 +4,10 @@ set -ex
 
 mkdir -p ./dist
 
-[-z VERSION] || VERSION=unknown
+VERSION=${VERSION:-unknown}
 
-CGO_ENABLED=0 GOARCH=amd64 GOOS=linux go build -ldflags="-s" -tags netgo -installsuffix netgo -o ./dist/goStatic-linux-amd64-${VERSION}
-CGO_ENABLED=0 GOARCH=arm GOARM=5 GOOS=linux go build -ldflags="-s" -tags netgo -installsuffix netgo -o ./dist/goStatic-linux-arm5-${VERSION}
-CGO_ENABLED=0 GOARCH=arm GOARM=6 GOOS=linux go build -ldflags="-s" -tags netgo -installsuffix netgo -o ./dist/goStatic-linux-arm6-${VERSION}
-CGO_ENABLED=0 GOARCH=arm GOARM=7 GOOS=linux go build -ldflags="-s" -tags netgo -installsuffix netgo -o ./dist/goStatic-linux-arm7-${VERSION}
-CGO_ENABLED=0 GOARCH=arm64 GOOS=linux go build -ldflags="-s" -tags netgo -installsuffix netgo -o ./dist/goStatic-linux-arm64-${VERSION}
+# CGO_ENABLED=0 GOARCH=amd64 GOOS=linux go build -ldflags="-s" -tags netgo -installsuffix netgo -o ./dist/goStatic-linux-amd64-${VERSION}
+# CGO_ENABLED=0 GOARCH=arm GOARM=5 GOOS=linux go build -ldflags="-s" -tags netgo -installsuffix netgo -o ./dist/goStatic-linux-arm5-${VERSION}
+# CGO_ENABLED=0 GOARCH=arm GOARM=6 GOOS=linux go build -ldflags="-s" -tags netgo -installsuffix netgo -o ./dist/goStatic-linux-arm6-${VERSION}
+# CGO_ENABLED=0 GOARCH=arm GOARM=7 GOOS=linux go build -ldflags="-s" -tags netgo -installsuffix netgo -o ./dist/goStatic-linux-arm7-${VERSION}
+# CGO_ENABLED=0 GOARCH=arm64 GOOS=linux go build -ldflags="-s" -tags netgo -installsuffix netgo -o ./dist/goStatic-linux-arm64-${VERSION}
