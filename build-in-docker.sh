90ff3b57965331183eb8a677c9dc9072151cd94d
Lucka <lpdrijfhout@gmail.com>
2020-11-26 12:00:08 +0200

    FIX build script

--- build-in-docker.sh
+++ build-in-docker.sh
@@ -2,6 +2,8 @@
 
 set -ex
 
-docker run --rm -it -v $PWD:/go/goStatic golang:alpine \
+env
+
+docker run --rm -v $PWD:/go/goStatic golang:alpine \
   sh -c \
     "cd /go/goStatic; sh build.sh; apk add upx; upx -9 dist/*"
