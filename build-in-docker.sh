9c8000ffc3d78f6fdc7d0c624614f6b27c415fbc
Lucka <lpdrijfhout@gmail.com>
2020-11-26 13:04:36 +0200

    ADD another github attempt 5

--- build-in-docker.sh
+++ build-in-docker.sh
@@ -2,6 +2,6 @@
 
 set -ex
 
-docker run --rm -e VERSION="${VERSION:-unknown}" -v $PWD:/go/goStatic golang:alpine \
+docker run --rm -v $PWD:/go/goStatic golang:alpine \
   sh -c \
     "cd /go/goStatic; sh build.sh; apk add upx; upx -9 dist/*"
--- build.sh
+++ build.sh
@@ -4,10 +4,8 @@ set -ex
 
 mkdir -p ./dist
 
-VERSION=${VERSION:-unknown}
-
-CGO_ENABLED=0 GOARCH=amd64 GOOS=linux go build -ldflags="-s" -tags netgo -installsuffix netgo -o ./dist/goStatic-linux-amd64-${VERSION}
-# CGO_ENABLED=0 GOARCH=arm GOARM=5 GOOS=linux go build -ldflags="-s" -tags netgo -installsuffix netgo -o ./dist/goStatic-linux-arm5-${VERSION}
-# CGO_ENABLED=0 GOARCH=arm GOARM=6 GOOS=linux go build -ldflags="-s" -tags netgo -installsuffix netgo -o ./dist/goStatic-linux-arm6-${VERSION}
-# CGO_ENABLED=0 GOARCH=arm GOARM=7 GOOS=linux go build -ldflags="-s" -tags netgo -installsuffix netgo -o ./dist/goStatic-linux-arm7-${VERSION}
-# CGO_ENABLED=0 GOARCH=arm64 GOOS=linux go build -ldflags="-s" -tags netgo -installsuffix netgo -o ./dist/goStatic-linux-arm64-${VERSION}
+CGO_ENABLED=0 GOARCH=amd64 GOOS=linux go build -ldflags="-s" -tags netgo -installsuffix netgo -o ./dist/goStatic-linux-amd64
+CGO_ENABLED=0 GOARCH=arm GOARM=5 GOOS=linux go build -ldflags="-s" -tags netgo -installsuffix netgo -o ./dist/goStatic-linux-arm5
+CGO_ENABLED=0 GOARCH=arm GOARM=6 GOOS=linux go build -ldflags="-s" -tags netgo -installsuffix netgo -o ./dist/goStatic-linux-arm6
+CGO_ENABLED=0 GOARCH=arm GOARM=7 GOOS=linux go build -ldflags="-s" -tags netgo -installsuffix netgo -o ./dist/goStatic-linux-arm7
+CGO_ENABLED=0 GOARCH=arm64 GOOS=linux go build -ldflags="-s" -tags netgo -installsuffix netgo -o ./dist/goStatic-linux-arm64
